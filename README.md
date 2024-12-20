86cea17b3ce2a9b1ae5e7b21a23323bc420908e5
임현호 <marvin.d@woowahan.com>
2024-12-21 00:40:09 +0900

    우형 이력서 업데이트

---
+++ README.md
@@ -0,0 +1,34 @@
+# 배포 전 할일
+
+1. 노션 내보내기 -> HTML
+
+2. HTML 파일 붙여넣기
+
+- `<!-- 아래에 노션 내보낸 html 붙여넣기 -->` 하위에 붙여넣기
+
+3. 프로필 이미지 경로 변경
+
+- `a` 태그, `img` 태그의 경로를 `"assets/profile.jpg"`로 변경
+
+4. 링크 경로 변경
+
+- `@ For English` -> `<a href="/en">`
+- `@ For Korean` -> `<a href="/">`
+
+5. 시간 계산 추가
+
+- body 안에 script 넣기
+  - 마지막줄에 `<script type="text/javascript" src="my.js">` 붙여넣기
+  - 최종 결과
+    - `<script type="text/javascript" src="my.js"></script></body></html>`
+- 년차 추가
+  - `<span id="my-career-ages"></span>` 추가하기
+  - 최종 결과
+    - `<span id="my-career-ages"></span>년차`
+    - `<span id="my-career-ages"></span> years as a developer`
+- 현재 회사 년차 추가
+  - 년: `<span id="my-current-company-years"></span>` 추가하기
+  - 월: `<span id="my-current-company-months"></span>` 추가하기
+  - 최종 결과
+    - `현재 (<span id="my-current-company-years"></span>년 <span id="my-current-company-months"></span>개월)`
+    - `Present (<span id="my-current-company-years"></span> years <span id="my-current-company-months"></span> months)`
--- my.js
+++ my.js
@@ -1,5 +1,32 @@
-const getAge = () => 1 + Math.floor((new Date() - new Date("2019-06-24").getTime()) / 3.15576e+10);
+// 첫 일 시작일: 2019년 6월 24일
+const getCareerAge = () => 1 + Math.floor((new Date() - new Date("2019-06-24").getTime()) / 3.15576e+10);
 
-// set age
-var element = document.getElementById("my-career-ages");
-element.innerText = getAge().toString();
+function calculateDuration(startYear, startMonth) {
+    // 현재 날짜
+    const today = new Date();
+    const currentYear = today.getFullYear();
+    const currentMonth = today.getMonth() + 1; // 월은 0부터 시작하므로 +1
+
+    // 입력 날짜
+    const startDate = new Date(startYear, startMonth - 1); // 월은 0부터 시작하므로 -1
+
+    // 년도와 월 계산
+    let years = currentYear - startYear;
+    let months = currentMonth - startMonth;
+
+    // 만약 월 차이가 음수라면, 이전 해로부터 차감
+    if (months < 0) {
+        years -= 1;
+        months += 12;
+    }
+
+    return { years, months };
+}
+
+// set career age
+document.getElementById("my-career-ages").innerText = getCareerAge().toString();
+
+// set current company age
+const { years, months } = calculateDuration(2022, 6); // 우아한형제들 2022년 6월 입사
+document.getElementById("my-current-company-years").innerText = years.toString();
+document.getElementById("my-current-company-months").innerText = months.toString();
